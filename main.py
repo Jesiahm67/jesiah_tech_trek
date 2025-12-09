fb286aca259216dc2a765bd586bdf3d5a6b6dfe6
Jesiah Minott <Jesiahm67@gmail.com>
2025-12-09 10:53:49 -0500

    Added images and need to add more products on my admin

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 from flask import Flask, render_template
 
+import pymysql
+
+from dynaconf import Dynaconf
+
 app = Flask(__name__)
 
+config = Dynaconf(settings_file=["settings.toml"])
 
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech",
+        user="jminott",
+        password=config.password,
+        database="jminott_pc_parts_and_service_",
+        autocommit=True,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    
+    return conn
 
 @app.route("/")
 def index():
     
     return render_template("homepage.html.jinja")
+@app.route("/browse")
+def browse():
+    connection = connect_db()
+    
+    cursor = connection.cursor()
+    
+    cursor.execute("Select * From `Product`")
+    
+    result = cursor.fetchall()
+    
+    connection.close()
+    
+    return render_template("browse.html.jinja")
